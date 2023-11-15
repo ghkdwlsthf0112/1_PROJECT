326718f0919b853f0ed0105a9854a0f1e0b3f9a6
User <User@192.168.45.116>
2023-11-15 22:14:47 +0900

    메인 화면 배경 수정

--- src/gui/layout/main/hotel_main/HotelMainLayout.java
+++ src/gui/layout/main/hotel_main/HotelMainLayout.java
@@ -22,19 +22,19 @@ import image.getImages;
 
 public class HotelMainLayout extends JFrame {
 
-	static ImageIcon mainImage = new getImages().getImageIcon(768, 1024, "src/image/hotel_image/델루나 메인.png");
+	static ImageIcon mainImage = new getImages().getImageIcon(768, 1024, "src/image/background_image/background4.png");
 	static ImageIcon checkInImage = new getImages().getImageIcon(710, 170, "src/image/icon_image/btn/checkIn.png");
 	static ImageIcon checkOutImage = new getImages().getImageIcon(340, 170, "src/image/icon_image/btn/checkOut.png");
 	static ImageIcon chkResvImage = new getImages().getImageIcon(340, 170,
 			"src/image/icon_image/btn/ConfirmationOfReservation.png");
-	static ImageIcon backImage = new getImages().getImageIcon(40, 40, "src/image/icon_image/btn/back.png");
-	static ImageIcon homeBtnImage = new getImages().getImageIcon(40, 40, "src/image/icon_image/btn/home.png");
-	static ImageIcon guideBtnImage = new getImages().getImageIcon(80, 40, "src/image/icon_image/btn/RoomInfomationMap.png");
+	static ImageIcon backImage = new getImages().getImageIcon(40, 40, "src/image/icon_image/btn/back_white.png");
+	static ImageIcon homeBtnImage = new getImages().getImageIcon(40, 40, "src/image/icon_image/btn/home_white.png");
+	static ImageIcon guideBtnImage = new getImages().getImageIcon(80, 40, "src/image/icon_image/btn/RoomInfomationMap_white.png");
 
 	static JButton b1 = new ChkInBtn(checkInImage);
 	static JButton b2 = new ChkOutBtn(checkOutImage);
 	static JButton b3 = new ChkReservationBtn(chkResvImage);
-	static JButton b4 = new AdminBtn("관리자버튼");
+	static JButton b4 = new AdminBtn();
 	static JButton b5 = new PrevBtn(backImage);
 	static JButton b6 = new HomeBtn(homeBtnImage);
 	static JButton b7 = new FloorGuideBtn(guideBtnImage);
@@ -48,15 +48,15 @@ public class HotelMainLayout extends JFrame {
 		imageLabel.setIcon(mainImage);
 
 		// 예약하기 버튼
-		b1.setBounds(25, 480, 700, 170);
+		b1.setBounds(25, 440, 700, 170);
 		imageLabel.add(b1);
 
 		// 체크아웃 버튼
-		b2.setBounds(25, 655, 340, 170);
+		b2.setBounds(25, 615, 340, 170);
 		imageLabel.add(b2);
 
 		// 예약확인 버튼
-		b3.setBounds(380, 655, 340, 170);
+		b3.setBounds(380, 615, 340, 170);
 		imageLabel.add(b3);
 
 		// 관리자 버튼 안보이는 버튼
