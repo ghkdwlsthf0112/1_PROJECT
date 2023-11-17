16f48ac4ab009a24ebb341fa9e21f15d40999d59
Gi7E-36 <Gi7E-36@DESKTOP-1NV2HPB>
2023-11-17 17:26:38 +0900

    환장하겠습니다

--- src/database/AdminDao.java
+++ src/database/AdminDao.java
@@ -8,25 +8,25 @@ import java.sql.SQLException;
 //DAO (Database Access Object) - DB에 쿼리문 보내고 결과 반영하는 객체
 public class AdminDao {
 	// 예약번호로 정보를 가져와 확인하는 메서드 예약이 있으면 1 없으면 -1
-	public int chkReservation(String reservation_number) {
-		int re = -1;
-		try (Connection conn = DBConnection.getConnection();) {
-			String sql = "SELECT * FROM reservation WHERE reservation_number LIKE '?'";
-			try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
-				pstmt.setString(1, "%"+reservation_number);
-				System.out.println(sql);
-				try(ResultSet rs = pstmt.executeQuery()){
-					if(rs.next()) {
-						System.out.println("나 여기 있어");
-						re = 1;			
-					}
-				}
-				return re;
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return re;
+	 public int chkReservation(String reservation_number) {
+	      int re = -1;
+	      try (Connection conn = DBConnection.getConnection();) {
+
+	         String sql = "SELECT * FROM reservation WHERE reservation_number LIKE ?";
+	         System.out.println(sql);
+	         try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+	            pstmt.setString(1,"%"+reservation_number);
+	            try (ResultSet rs = pstmt.executeQuery()) {
+	               if (rs.isFirst()== true) {
+	                  System.out.println(re);
+	               }
+	                  System.out.println(re);
+	            }
+	         }
+	      } catch (SQLException e) {
+	         e.printStackTrace();
+	      }
+	      return re;
 	}
 	// 예약정보를 가져와 확인후 그 방의 체크인 상태를 바꿔주는 메서드
 	// 예약정보를 가져와 확인후 그 방을 체크아웃 시켜주는 메서드
--- src/database/reservation/Reservation.java
+++ src/database/reservation/Reservation.java
@@ -22,4 +22,54 @@ public class Reservation {
 		this.reservation_start = reservation_start;
 		this.reservation_end = reservation_end;
 	}
+
+	public Integer getReservation_id() {
+		return reservation_id;
+	}
+
+	public void setReservation_id(Integer reservation_id) {
+		this.reservation_id = reservation_id;
+	}
+
+	public String getReservation_number() {
+		return reservation_number;
+	}
+
+	public void setReservation_number(String reservation_number) {
+		this.reservation_number = reservation_number;
+	}
+
+	public String getCustomer_email() {
+		return customer_email;
+	}
+
+	public void setCustomer_email(String customer_email) {
+		this.customer_email = customer_email;
+	}
+
+	public Integer getRoom_number() {
+		return room_number;
+	}
+
+	public void setRoom_number(Integer room_number) {
+		this.room_number = room_number;
+	}
+
+	public String getReservation_start() {
+		return reservation_start;
+	}
+
+	public void setReservation_start(String reservation_start) {
+		this.reservation_start = reservation_start;
+	}
+
+	public String getReservation_end() {
+		return reservation_end;
+	}
+
+	public void setReservation_end(String reservation_end) {
+		this.reservation_end = reservation_end;
+	}
+	
+	
 }
